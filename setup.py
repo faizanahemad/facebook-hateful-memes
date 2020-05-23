2fad689bba2b87bf0397eec3b02d7e4ff24d4768
Faizan Ahemad <ahemf@amazon.com>
2020-05-24 00:59:58 +0530

    packaging

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-setup(name='facebook_hateful_memes',
+setup(name='facebook_memes',
       version='0.0.1',
       description='',
       url='https://github.com/faizanahemad/facebook-hateful-memes',
