b6cbc98aa49baea3cad2cbd25a89b1c887b54725
Faizan Ahemad <ahemf@amazon.com>
2020-06-21 11:22:15 +0530

    Detectron Features based Attention Support

--- facebook_hateful_memes_detector/models/MultiModal/VilBertVisualBert.py
+++ facebook_hateful_memes_detector/models/MultiModal/VilBertVisualBert.py
@@ -222,6 +222,8 @@ class VilBertVisualBertModel(nn.Module):
 
         sample_list.position_embeddings_visual = None
 
+        sample_list.image_mask = sample_list.image_mask.to(get_device())
+        sample_list.input_mask = sample_list.input_mask.to(get_device())
         sample_list = self.model.flatten_for_bert(sample_list)
 
         sequence_output, pooled_output, attention_weights = self.model.model.bert(
