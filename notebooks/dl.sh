603482ebfd73f27113e92be10ec875c99643d2f3
Faizan Ahemad <ahemf@amazon.com>
2020-06-25 18:40:32 +0530

    Adding Fasttext download

---
+++ notebooks/dl.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+
+wget https://dl.fbaipublicfiles.com/fasttext/vectors-english/crawl-300d-2M-subword.zip
+unzip wiki-news-300d-1M-subword.bin.zip
+
+wget https://dl.fbaipublicfiles.com/fasttext/vectors-english/wiki-news-300d-1M-subword.bin.zip
+unzip wiki-news-300d-1M-subword.bin.zip
+
