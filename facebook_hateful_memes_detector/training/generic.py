adf822d85bf08be33f292d0c6ff2b7f1139eb5c1
Faizan Ahemad <ahemf@amazon.com>
2020-06-26 00:57:57 +0530

    Bug fix

--- facebook_hateful_memes_detector/training/generic.py
+++ facebook_hateful_memes_detector/training/generic.py
@@ -115,7 +115,7 @@ def train(model, optimizer, scheduler_init_fn, batch_size, epochs, dataset, vali
         color = 'tab:red'
         ax1.set_xlabel('Training Batches')
         ax1.set_ylabel('Loss', color=color)
-        ax1.enable_plot(t, train_losses, color=color)
+        ax1.plot(t, train_losses, color=color)
         ax1.tick_params(axis='y', labelcolor=color)
 
         ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
