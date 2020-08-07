e29bac3bcfffc4ee5b99732f95b2940432275729
Faizan Ahemad <ahemf@amazon.com>
2020-08-08 00:18:58 +0530

    Bug fix

--- facebook_hateful_memes_detector/training/generic.py
+++ facebook_hateful_memes_detector/training/generic.py
@@ -517,8 +517,12 @@ def train_for_augment_similarity(model, optimizer, scheduler_init_fn,
                             clean_memory()
                     else:
                         repr = model(augmented_batch)
+                        if isinstance(repr, (list, tuple)):
+                            repr = repr[-1]
                         with torch.no_grad():
                             orig_repr = orig_model(batch)
+                            if isinstance(orig_repr, (list, tuple)):
+                                orig_repr = orig_repr[-1]
                         loss = ((repr - orig_repr) ** 2).mean()
                         loss = loss / accumulation_steps
                         loss.backward()
