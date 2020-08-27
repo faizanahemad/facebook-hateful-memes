826d4ea8aaaae0a2efc01a17a9259857007a0334
Faizan Ahemad <ahemf@amazon.com>
2020-08-27 17:57:59 +0530

    Bug fix

--- facebook_hateful_memes_detector/preprocessing/__init__.py
+++ facebook_hateful_memes_detector/preprocessing/__init__.py
@@ -697,7 +697,7 @@ class TextAugment:
 
 def get_image2torchvision_transforms():
     preprocess = transforms.Compose([
-        transforms.Resize(224),
+        transforms.Resize((224, 224)),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
