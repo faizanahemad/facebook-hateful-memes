a375d37e095dc5b9d7b5e8991b574a6e2d9fdc33
Faizan Ahemad <ahemf@amazon.com>
2020-10-11 02:25:21 +0530

    MLMv2

--- facebook_hateful_memes_detector/models/MultiModal/__init__.py
+++ facebook_hateful_memes_detector/models/MultiModal/__init__.py
@@ -1,2 +1,2 @@
 from .VilBertVisualBert import VilBertVisualBertModel
-from .VilBertVisualBertV2 import VilBertVisualBertModelV2, MLMSimCLR
+from .VilBertVisualBertV2 import VilBertVisualBertModelV2, MLMSimCLR, MLMOnly, MLMOnlyV2
