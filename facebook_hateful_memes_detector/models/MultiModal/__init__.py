dec20d626342634fd7d5b2aa348a65fbc8b9d8ae
Faizan Ahemad <ahemf@amazon.com>
2020-10-21 15:17:34 +0530

    Bug fix

--- facebook_hateful_memes_detector/models/MultiModal/__init__.py
+++ facebook_hateful_memes_detector/models/MultiModal/__init__.py
@@ -1,2 +1,2 @@
 from .VilBertVisualBert import VilBertVisualBertModel
-from .VilBertVisualBertV2 import VilBertVisualBertModelV2, MLMSimCLR, MLMOnly, MLMOnlyV2, make_plots
+from .VilBertVisualBertV2 import VilBertVisualBertModelV2, MLMSimCLR, MLMOnlyV2, make_plots
