4890594c5b400c6c726a25d730e3a4c701191da4
Faizan Ahemad <ahemf@amazon.com>
2020-10-30 19:43:34 +0530

    Bug fix

--- facebook_hateful_memes_detector/models/text_models/BERTClassifier.py
+++ facebook_hateful_memes_detector/models/text_models/BERTClassifier.py
@@ -25,28 +25,28 @@ class BERTClassifier(nn.Module):
                  internal_dims, n_layers,
                  device,
                  n_tokens_in=64, n_tokens_out=16,
-                 use_as_super=False, **kwargs):
+                 **kwargs):
         embedding_dims = 768
         super(BERTClassifier, self).__init__()
         self.word_masking_proba = kwargs["word_masking_proba"] if "word_masking_proba" in kwargs else 0.0
+        self.n_tokens_in = n_tokens_in
 
-        if not use_as_super:
-            model = kwargs["model"] if "model" in kwargs else 'albert-base-v2'
-            global_dir = get_global("models_dir")
-            model = os.path.join(global_dir, model) if model in os.listdir(global_dir) else model
-            self.tokenizer = AutoTokenizer.from_pretrained(model)
-            self.model = AutoModel.from_pretrained(model)
-            print("Pick stored Model", model, "Model Class = ", type(self.model), "Tokenizer Class = ", type(self.tokenizer))
+        model = kwargs["model"] if "model" in kwargs else 'albert-base-v2'
+        global_dir = get_global("models_dir")
+        model = os.path.join(global_dir, model) if model in os.listdir(global_dir) else model
+        self.tokenizer = AutoTokenizer.from_pretrained(model)
+        self.model = AutoModel.from_pretrained(model)
+        print("Pick stored Model", model, "Model Class = ", type(self.model), "Tokenizer Class = ", type(self.tokenizer))
 
-            self.attention_drop_proba = kwargs["attention_drop_proba"] if "attention_drop_proba" in kwargs else 0.0
-            n_encoders = kwargs.pop("n_encoders", n_layers)
-            n_decoders = kwargs.pop("n_decoders", n_layers)
-            self.featurizer = TransformerFeaturizer(n_tokens_in, embedding_dims, n_tokens_out,
-                                                    classifier_dims,
-                                                    internal_dims, n_encoders, n_decoders,
-                                                    gaussian_noise, dropout, self.attention_drop_proba)
+        self.attention_drop_proba = kwargs["attention_drop_proba"] if "attention_drop_proba" in kwargs else 0.0
+        n_encoders = kwargs.pop("n_encoders", n_layers)
+        n_decoders = kwargs.pop("n_decoders", n_layers)
+        self.featurizer = TransformerFeaturizer(n_tokens_in, embedding_dims, n_tokens_out,
+                                                classifier_dims,
+                                                internal_dims, n_encoders, n_decoders,
+                                                gaussian_noise, dropout, self.attention_drop_proba)
 
-            self.final_layer = fb_1d_loss_builder(classifier_dims, n_tokens_out, num_classes, dropout, **kwargs)
+        self.final_layer = fb_1d_loss_builder(classifier_dims, n_tokens_out, num_classes, dropout, **kwargs)
         if "stored_model" in kwargs:
             load_stored_params(self, kwargs["stored_model"])
         self.word_masking = WordMasking(tokenizer=self.tokenizer, **kwargs)
