8bbdac7b3420e37adbe5f4256399f4d38afa7eb6
Faizan Ahemad <ahemf@amazon.com>
2020-10-31 14:05:24 +0530

    Bug fix

--- facebook_hateful_memes_detector/models/text_models/BERTClassifier.py
+++ facebook_hateful_memes_detector/models/text_models/BERTClassifier.py
@@ -29,7 +29,9 @@ class BERTClassifier(nn.Module):
         embedding_dims = 768
         super(BERTClassifier, self).__init__()
         self.word_masking_proba = kwargs["word_masking_proba"] if "word_masking_proba" in kwargs else 0.0
+        self.mlm_probability = self.word_masking_proba
         self.n_tokens_in = n_tokens_in
+        self.token_cache = kwargs.pop("token_cache", None)
 
         model = kwargs["model"] if "model" in kwargs else 'albert-base-v2'
         global_dir = get_global("models_dir")
@@ -52,13 +54,54 @@ class BERTClassifier(nn.Module):
         self.word_masking = WordMasking(tokenizer=self.tokenizer, **kwargs)
         self.device=device
 
-    def tokenise(self, texts: List[str]):
+    def mask_tokens(self, inputs: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        """
+        Prepare masked tokens inputs/labels for masked language modeling: 80% MASK, 10% random, 10% original.
+        """
+
+        if self.tokenizer.mask_token is None:
+            raise ValueError(
+                "This tokenizer does not have a mask token which is necessary for masked language modeling. Remove the --mlm flag if you want to use this tokenizer."
+            )
+
+        labels = inputs.clone()
+        # We sample a few tokens in each sequence for masked-LM training (with probability args.mlm_probability defaults to 0.15 in Bert/RoBERTa)
+        probability_matrix = torch.full(labels.shape, self.mlm_probability)
+        special_tokens_mask = [
+            self.tokenizer.get_special_tokens_mask(val, already_has_special_tokens=True) for val in labels.tolist()
+        ]
+        probability_matrix.masked_fill_(torch.tensor(special_tokens_mask, dtype=torch.bool), value=0.0)
+        if self.tokenizer._pad_token is not None:
+            padding_mask = labels.eq(self.tokenizer.pad_token_id)
+            probability_matrix.masked_fill_(padding_mask, value=0.0)
+        masked_indices = torch.bernoulli(probability_matrix).bool()
+        labels[~masked_indices] = -100  # We only compute loss on masked tokens
+
+        # 80% of the time, we replace masked input tokens with tokenizer.mask_token ([MASK])
+        indices_replaced = torch.bernoulli(torch.full(labels.shape, 0.8)).bool() & masked_indices
+        inputs[indices_replaced] = self.tokenizer.convert_tokens_to_ids(self.tokenizer.mask_token)
+
+        # 10% of the time, we replace masked input tokens with random word
+        indices_random = torch.bernoulli(torch.full(labels.shape, 0.5)).bool() & masked_indices & ~indices_replaced
+        random_words = torch.randint(len(self.tokenizer), labels.shape, dtype=torch.long)
+        inputs[indices_random] = random_words[indices_random]
+
+        # The rest of the time (10% of the time) we keep the masked input tokens unchanged
+        return inputs, labels
+
+    def tokenise(self, ids, texts: List[str]):
         tokenizer = self.tokenizer
         n_tokens_in = self.n_tokens_in
-        texts = self.word_masking(texts)
-        converted_texts = tokenizer.batch_encode_plus(texts, add_special_tokens=True, pad_to_max_length=True, max_length=n_tokens_in, truncation=True)
-        input_ids, attention_mask = converted_texts["input_ids"], converted_texts["attention_mask"]
-        return torch.tensor(input_ids).to(self.device), torch.tensor(attention_mask).to(self.device)
+        if self.token_cache is None:
+            converted_texts = tokenizer.batch_encode_plus(texts, add_special_tokens=True, pad_to_max_length=True, max_length=n_tokens_in, truncation=True)
+            input_ids, attention_mask = converted_texts["input_ids"], converted_texts["attention_mask"]
+        else:
+            input_ids, attention_mask = zip(*[self.token_cache[id] for id in ids])
+
+        input_ids, attention_mask = torch.tensor(input_ids), torch.tensor(attention_mask)
+        input_ids, _ = self.mask_tokens(input_ids)
+        return input_ids.to(self.device), attention_mask.to(self.device)
+
 
     def get_word_vectors(self, input_ids, attention_mask):
         outputs = self.model(input_ids, attention_mask=attention_mask)
